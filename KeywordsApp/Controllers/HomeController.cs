3277df01a299cc4f9a108616b5997519ac64eb3a
Xavier MALPARTY <xavier@malparty.fr>
2021-05-03 10:43:18 +0700

    [#70] Sleep, change query URL and HttpClient Dispose trial in Demo

--- KeywordsApp/Controllers/HomeController.cs
+++ KeywordsApp/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace keywords.Controllers
             // IMPROVEMENT: can have 2 threads (one using proxy, the other one direct google?)
             //
             //
+            const int MAX_RETRY = 10;
+            const int INIT_SLEEP_TIME = 500; // ms
+            const int MIN_RETRY_SLEEP_TIME = 250; //ms
+            int sleepTime = INIT_SLEEP_TIME;
 
             var searchResults = new List<KeyResult>();
             var queryList = new string[]{"bonjour", "palace","minister","outline",
@@ -64,21 +68,41 @@ namespace keywords.Controllers
                             "eu2","us1","us2","us3","us4","us5","us6","us7","us8","us9","us10",
                             "us11","us12","us13","us14","us15","us16","us17","eu1","eu2","eu3","eu4","eu5","eu6",
                             "eu7","eu8","eu9","eu10","eu11","eu12","eu13","eu14","eu15"};
-            using (HttpClient client = new HttpClient())
+            HttpClient client = new HttpClient();
+            try
             {
-
-
+                var googleIndex = 0;
                 foreach (var keyword in queryList.Take(100)) // .Take(1)
                 {
                     Console.WriteLine("Parsing new keyword: " + keyword);
-                    foreach (var proxyServer in proxyServers)
-                    {
-                        Console.WriteLine("Server: " + proxyServer);
+                    // foreach (var proxyServer in proxyServers)
+                    // {
+                    //     Console.WriteLine("Server: " + proxyServer);
 
-                        // Call proxy with Server arg and Google query:
-                        const string GOOGLE_URI = "https://www.google.com/search";
-                        var queryGoogle = GOOGLE_URI + "?q=" + keyword;
-                        var proxyUrl = "https://" + proxyServer + ".proxysite.com/includes/process.php?action=update";
+                    // Call proxy with Server arg and Google query:
+                    var googleUrls = new string[] {"https://www.google.com/search",
+                    "https://www.google.com.vn/search"
+                    }
+                    ;
+                    var queryGoogle = googleUrls[googleIndex] + "?q=" + keyword;
+                    // var proxyUrl = "https://" + proxyServer + ".proxysite.com/includes/process.php?action=update";
+
+
+
+                    // // Set proxy form submition content
+                    // var formContent = new FormUrlEncodedContent(new[]
+                    // {
+                    //     // new KeyValuePair<string, string>("q", keyword), // google
+
+                    //     new KeyValuePair<string, string>("d", queryGoogle), // proxy
+                    //     new KeyValuePair<string, string>("server-option", proxyServer), // proxy
+                    // });
+                    // request.Content = formContent;
+
+                    int retryCount = 0;
+                    HttpResponseMessage respProxy = null;
+                    do
+                    {
 
                         // Build request:
                         var request = new HttpRequestMessage(HttpMethod.Get, queryGoogle);
@@ -107,52 +131,76 @@ namespace keywords.Controllers
                         // Set language to english
                         request.Headers.AcceptLanguage.ParseAdd("en-US");
 
+                        respProxy = client.SendAsync(request, cancellationToken: CancellationToken.None).Result;
+                        // Preventing error 429 - Too many requests
+                        Thread.Sleep(sleepTime);
+                        retryCount++;
 
-                        // // Set proxy form submition content
-                        // var formContent = new FormUrlEncodedContent(new[]
-                        // {
-                        //     // new KeyValuePair<string, string>("q", keyword), // google
-
-                        //     new KeyValuePair<string, string>("d", queryGoogle), // proxy
-                        //     new KeyValuePair<string, string>("server-option", proxyServer), // proxy
-                        // });
-                        // request.Content = formContent;
-
-                        HttpResponseMessage respProxy = client.SendAsync(request, cancellationToken: CancellationToken.None).Result;
+                        // Faster if Success, slower if failed
                         if (respProxy.IsSuccessStatusCode)
+                            sleepTime = sleepTime / 2;
+                        else if (sleepTime > 1100)
                         {
-                            searchResults.Add(new KeyResult
+                            if (googleIndex == 0)
                             {
-                                Keyword = keyword,
-                                PageContent = await respProxy.Content.ReadAsStringAsync()
-                            });
-                            break; // No need to try with another server
-                        }
-                        // Too many requests - bot detected
-                        else if ((int)respProxy.StatusCode == 429)
-                        {
-                            Console.WriteLine("Proxy send '429 (Too many requests)");
-                            Console.WriteLine(respProxy.Content);
-                            Console.WriteLine(respProxy.ReasonPhrase);
-                            Console.WriteLine(respProxy.Headers);
-                            Console.WriteLine("Trying another server");
-                            continue;
+                                googleIndex = 1;
+                                Console.WriteLine("Trying with Google Vietnam");
+                                continue; // Retry
+                            }
+                            else
+                            {
+                                client.Dispose();
+                                client = new HttpClient();
+                                Console.WriteLine("Trying to dispose HttpClient");
+                                continue; // Retry
+                            }
                         }
                         else
-                        {
-                            // Need to try with another server
-                            Console.WriteLine("Parsing Proxy unsufessful:");
-                            Console.WriteLine("{0} ({1})", (int)respProxy.StatusCode, respProxy.ReasonPhrase);
-                            continue;
-                        }
+                            sleepTime = sleepTime < MIN_RETRY_SLEEP_TIME ? MIN_RETRY_SLEEP_TIME : sleepTime * 2;
+                        Console.WriteLine("New sleep time: " + sleepTime);
                     }
+                    while (retryCount < MAX_RETRY && !respProxy.IsSuccessStatusCode);
 
-                    // Preventing error 429 - Too many requests
-                    Thread.Sleep(3600);
+                    if (respProxy.IsSuccessStatusCode)
+                    {
+                        searchResults.Add(new KeyResult
+                        {
+                            Keyword = keyword,
+                            PageContent = await respProxy.Content.ReadAsStringAsync()
+                        });
+                        // break; // No need to try with another server
+                        Console.WriteLine("GOOD!!");
 
-
+                    }
+                    // Too many requests - bot detected
+                    else if ((int)respProxy.StatusCode == 429)
+                    {
+                        Console.WriteLine("Google send '429 (Too many requests)");
+                        Console.WriteLine(respProxy.Content);
+                        Console.WriteLine(respProxy.ReasonPhrase);
+                        Console.WriteLine(respProxy.Headers);
+                        Console.WriteLine("EXPERIMENTATION FAILED!!");
+                    }
+                    else
+                    {
+                        // Need to try with another server
+                        Console.WriteLine("EXPERIMENTATION FAILED!!");
+                        Console.WriteLine("Parsing Proxy unsufessful:");
+                        Console.WriteLine("{0} ({1})", (int)respProxy.StatusCode, respProxy.ReasonPhrase);
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.Error.Write(e.Message);
+                Console.Error.Write(e.Data);
+                // Mark current object status as failed.
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            // Persist current data changes
 
             return View(searchResults);
         }
