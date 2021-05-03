4d20875ca08ab1ad5c2403f2cc35e8511a71c939
Xavier MALPARTY <xavier@malparty.fr>
2021-05-03 14:34:55 +0700

    [#42] Add CSV File base model

---
+++ KeywordsApp/Models/File.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeywordsApp.Models
+{
+
+    [Table("Users")]
+    [Index(nameof(CreatedDate), nameof(Name))]
+    public class File
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "character varying(256)")]
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public IEnumerable<Keyword> Keywords { get; set; }
+
+    }
+}
