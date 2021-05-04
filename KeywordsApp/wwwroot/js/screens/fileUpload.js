4783124ba4847f3edc714417b5e1cfa8d87014d8
Xavier MALPARTY <xavier@malparty.fr>
2021-05-04 09:24:49 +0700

    [#48] Add client-side form validation and alerts.

--- KeywordsApp/wwwroot/js/screens/fileUpload.js
+++ KeywordsApp/wwwroot/js/screens/fileUpload.js
@@ -1,24 +1,17 @@
 $(function () {
-  // init toasts
-  var toastElList = [].slice.call(document.querySelectorAll('.toast'));
-  const toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl, {
-      animation: true,
-      autohide: false,
-      delay: 5000,
-    });
-  });
-
-  const MAX_UPLOAD_SIZE = 1; // Mb
+  const MAX_UPLOAD_SIZE = 10000; // 10kb
   const form = $('.csv-form');
   form.change((e, f) => {
     const errorMsgElt = form.find('.csv-form-error-msg');
     const fileNameElt = form.find('.csv-form-file-name');
     const csvFileInput = form.find('.csv-form-file-input');
-
+    const uploadedMsgElt = form.find('.csv-form-uploaded-msg');
     // Reset texts
+    errorMsgElt.hide();
+    uploadedMsgElt.hide();
     errorMsgElt.html('');
     fileNameElt.html('');
+    fileNameElt.removeClass('text-danger');
 
     if (csvFileInput[0].files.length > 0) {
       const file = csvFileInput[0].files[0];
@@ -26,20 +19,23 @@ $(function () {
       // Limit to less than 1mb
       if (file.size > MAX_UPLOAD_SIZE) {
         // File too big.
-        errorMsgElt.html(`Please select a file under ${MAX_UPLOAD_SIZE}Mb.`);
-        csvFileInput.addClass('is-invalid');
-        // errorMsgElt.show();
-        return;
+        errorMsgElt.html(
+          `Please select a file under ${MAX_UPLOAD_SIZE / 10000}Kb.`
+        );
+        errorMsgElt.show(250);
+        fileNameElt.addClass('text-danger');
       }
       // Ensure extension is '.csv'
       else if (file.name.match(/\.[0-9a-z]+$/i)[0] != '.csv') {
         // Wrong file extension
-        alert('EXTENSION');
-        return;
+        errorMsgElt.html(`Please select a .csv file.`);
+        errorMsgElt.show(250);
+        fileNameElt.addClass('text-danger');
+      } else {
+        // Run Post request
+        uploadedMsgElt.show(250);
       }
       fileNameElt.html(file.name);
-      toastList[0].show();
-      // $('#fileUploadToast').show(250);
     }
     // check file extension and type + limit in INPUT  first
     // Send upload
