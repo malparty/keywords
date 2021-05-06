094e8f37197f1cb7f51a462dba48a0755f24cf4b
Xavier MALPARTY <xavier@malparty.fr>
2021-05-06 10:32:19 +0700

    [#39] Improve UI for keyword listing. Refactor code for group by order key.

--- KeywordsApp/Models/Keyword/KeywordViewModel.cs
+++ KeywordsApp/Models/Keyword/KeywordViewModel.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using X.PagedList;
+using X.PagedList.Web.Common;
 
 namespace KeywordsApp.Models.Keyword
 {
@@ -7,6 +10,31 @@ namespace KeywordsApp.Models.Keyword
     {
         public KeywordOrderBy OrderBy { get; set; }
         public IPagedList<KeywordEntity> Keywords { get; set; }
+        public PagedListRenderOptionsBase PagedListRenderOptions
+        {
+            get
+            {
+                return new X.PagedList.Web.Common.PagedListRenderOptionsBase
+                {
+                    UlElementClasses = new string[] { "pagination" },
+                    LiElementClasses = new string[] { "page-item" },
+                    ActiveLiElementClass = "active",
+                    PageClasses = new string[] { "page-link" }
+                };
+            }
+        }
+
+        // based on orderby, group using the right key
+        public IEnumerable<IGrouping<string, KeywordEntity>> GroupedByOrderKey
+        {
+            get
+            {
+                if (OrderBy == KeywordOrderBy.Status)
+                    return Keywords.GroupBy(x => x.ParsingStatus.ToString());
+                else
+                    return Keywords.GroupBy(x => x.Name.ToLower()[0].ToString());
+            }
+        }
     }
     public enum KeywordOrderBy
     {
