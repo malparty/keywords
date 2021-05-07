6e3aab4700b639c5697f812963092f1ae02b69ce
Xavier MALPARTY <xavier@malparty.fr>
2021-05-07 10:11:24 +0700

    [#24] Add parsing Model and update KeywordEntity with parsing data

--- KeywordsApp/Models/Keyword/KeywordEntity.cs
+++ KeywordsApp/Models/Keyword/KeywordEntity.cs
@@ -19,6 +19,15 @@ namespace KeywordsApp.Models.Keyword
         public DateTime? ParsedDate { get; set; }
         public ParsingStatus ParsingStatus { get; set; }
 
+        public int AdWordsCount { get; set; }
+        public int LinkCount { get; set; }
+
+        public int TotalThouthandResultsCount { get; set; }
+        public string HtmlCode { get; set; }
+
+        // Miliseconds
+        public int RequestDuration { get; set; }
+
         public int FileId { get; set; }
         public FileEntity File { get; set; }
     }
---
+++ KeywordsApp/Models/Keyword/KeywordParserModel.cs
@@ -0,0 +1,11 @@
+namespace KeywordsApp.Models.Keyword
+{
+    public class KeywordParserModel
+    {
+        public int KeywordId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+    }
+
+
+}
