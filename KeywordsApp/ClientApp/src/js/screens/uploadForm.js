4e00267edd8bf6d852fa4db4d327cbc381f1274c
Xavier MALPARTY <xavier@malparty.fr>
2021-05-16 09:39:35 +0700

    [gh#31] Refactor UploadForm.js to benefit from classes

--- KeywordsApp/ClientApp/src/js/screens/uploadForm.js
+++ KeywordsApp/ClientApp/src/js/screens/uploadForm.js
@@ -1,49 +1,57 @@
 class UploadForm {
   MAX_UPLOAD_SIZE = 10000; // 10kb
-  form = $('.csv-form');
-  initForm = () => {
-    this.form.change(() => {
-      const errorMsgElt = this.form.find('.csv-form-error-msg');
-      const fileNameElt = this.form.find('.csv-form-file-name');
-      const csvFileInput = this.form.find('.csv-form-file-input');
-      const uploadedMsgElt = this.form.find('.csv-form-uploaded-msg');
-      // Reset texts
-      errorMsgElt.fadeOut();
-      uploadedMsgElt.fadeOut();
-      errorMsgElt.html('');
-      fileNameElt.html('');
-      fileNameElt.removeClass('text-danger');
+  form = null;
+  constructor(form) {
+    if (form.length > 0 && form.prop('nodeName') === 'FORM') {
+      this.form = form;
+      form.on('change', this.onFormChange);
+    } else {
+      console.error('UploadForm needs a <form> object to work properly.');
+    }
+  }
 
-      if (csvFileInput[0].files.length > 0) {
-        const file = csvFileInput[0].files[0];
+  onFormChange = () => {
+    const errorMsgElt = this.form.find('.csv-form-error-msg');
+    const fileNameElt = this.form.find('.csv-form-file-name');
+    const csvFileInput = this.form.find('.csv-form-file-input');
+    const uploadedMsgElt = this.form.find('.csv-form-uploaded-msg');
+    // Reset texts
+    errorMsgElt.fadeOut();
+    uploadedMsgElt.fadeOut();
+    errorMsgElt.html('');
+    fileNameElt.html('');
+    fileNameElt.removeClass('text-danger');
 
-        // Limit to less than 1mb
-        if (file.size > this.MAX_UPLOAD_SIZE) {
-          // File too big.
-          errorMsgElt.html(
-            `Please select a file under ${this.MAX_UPLOAD_SIZE / 10000}Kb.`
-          );
-          errorMsgElt.fadeIn(250);
-          fileNameElt.addClass('text-danger');
-        }
-        // Ensure extension is '.csv'
-        else if (file.name.match(/\.[0-9a-z]+$/i)[0] != '.csv') {
-          // Wrong file extension
-          errorMsgElt.html(`Please select a .csv file.`);
-          errorMsgElt.fadeIn(250);
-          fileNameElt.addClass('text-danger');
-        } else {
-          uploadedMsgElt.fadeIn(250);
-          // Submit form
-          this.submitForm();
-        }
-        fileNameElt.html(file.name);
+    if (csvFileInput[0].files.length > 0) {
+      const file = csvFileInput[0].files[0];
+
+      // Limit to less than 1mb
+      if (file.size > this.MAX_UPLOAD_SIZE) {
+        // File too big.
+        errorMsgElt.html(
+          `Please select a file under ${this.MAX_UPLOAD_SIZE / 10000}Kb.`
+        );
+        errorMsgElt.fadeIn(250);
+        fileNameElt.addClass('text-danger');
       }
-    });
+      // Ensure extension is '.csv'
+      else if (file.name.match(/\.[0-9a-z]+$/i)[0] != '.csv') {
+        // Wrong file extension
+        errorMsgElt.html(`Please select a .csv file.`);
+        errorMsgElt.fadeIn(250);
+        fileNameElt.addClass('text-danger');
+      } else {
+        uploadedMsgElt.fadeIn(250);
+        // Submit Ajax form
+        this.submitAjax();
+      }
+      fileNameElt.html(file.name);
+    }
   };
 
-  submitForm = () => {
+  submitAjax = () => {
     const data = new FormData(this.form[0]);
+    const thisForm = this.form;
     $.ajax({
       type: 'POST',
       enctype: 'multipart/form-data',
@@ -54,12 +62,13 @@ class UploadForm {
       cache: false,
       timeout: 600000,
       success: function (data) {
-        $('#uploadFormContainer').html(data);
-        const uploadForm = new UploadForm();
-        uploadForm.initForm();
-        const newFileId = $('#previousFileId').val();
+        const container = thisForm.parent();
+        container.html(data);
+        const newForm = container.children('form');
+        const uploadForm = new UploadForm(newForm);
+
         // Load newly created card
-        uploadForm.loadNewCard(newFileId);
+        uploadForm.loadNewCard();
       },
       error: function (e) {
         console.log('ERROR : ', e);
@@ -67,7 +76,12 @@ class UploadForm {
     });
   };
 
-  loadNewCard = (newFileId) => {
+  loadNewCard = () => {
+    // only if no error msg
+    const newFileId = $('#previousFileId').val();
+    if (newFileId === '0') {
+      return;
+    }
     const url = $('#csvFilesList').data('single-load');
     const container = $('#csvFilesListContainer');
     $.post(url, { fileId: newFileId }, (data) => {
@@ -87,5 +101,6 @@ class UploadForm {
   };
 }
 $(function () {
-  new UploadForm().initForm();
+  const form = $('.csv-form');
+  new UploadForm(form);
 });
--- KeywordsApp/ClientApp/src/js/site.js
+++ KeywordsApp/ClientApp/src/js/site.js
@@ -8,7 +8,3 @@ import 'jquery-ui/ui/effects/effect-highlight';
 import '../js/components/parser-updates.js';
 import '../js/components/loader-ajax.js';
 import '../js/screens/uploadForm.js';
-
-$(() => {
-  console.log($.ui);
-});
